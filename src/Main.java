bbb40b3138a6a21738bd33be4577a3e6d995269c
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-10-23 15:40:17 +0800

    m2q6

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        float n = scn.nextFloat();
-        float max = n;
-        float min = n;
-        for( int i =1;i<=10;i++){
-            n = scn.nextFloat();
-            if (max<n){
-                max=n;
-            }else if (min>n){
-                min = n;
+        int n = scn.nextInt();
+        int m = n/2;
+        boolean flag = true;
+        for( int i =2;i<=m;i++){
+            if (n%i==0){
+                flag = false;
+                break;
             }
         }
-        System.out.println(max);
-        System.out.println(min);
+        if (flag) {
+            System.out.println("yes");
+        }else{System.out.println("no");
+        }
     }
 }
